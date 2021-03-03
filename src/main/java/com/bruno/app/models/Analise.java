a63486a69a3d477e584621d4884f7180c4d71642
Bruno Ricardo Secco <brunoricardosecco@hotmail.com>
2021-03-02 23:56:23 -0300

    feat: adicao de modelo de Teores

--- src/main/java/com/bruno/app/models/Analise.java
+++ src/main/java/com/bruno/app/models/Analise.java
@@ -12,13 +12,6 @@ public class Analise {
     private Integer sistemaCultivo;
     private Usuario responsavelTecnico;
     private Integer numResultadoAnaliseSolos;
-    private Double fosforo;
-    private Double potassio;
-    private Double calcio;
-    private Double magnesio;
-    private Double enxofre;
-    private Double aluminio;
-    private Double HAL;
     private Double fosforoIdeal;
     private Double potassioIdeal;
     private Double calcioIdeal;
@@ -96,62 +89,6 @@ public class Analise {
 
     public void setNumResultadoAnaliseSolos(Integer numResultadoAnaliseSolos) {
         this.numResultadoAnaliseSolos = numResultadoAnaliseSolos;
-    }
-
-    public Double getFosforo() {
-        return fosforo;
-    }
-
-    public void setFosforo(Double fosforo) {
-        this.fosforo = fosforo;
-    }
-
-    public Double getPotassio() {
-        return potassio;
-    }
-
-    public void setPotassio(Double potassio) {
-        this.potassio = potassio;
-    }
-
-    public Double getCalcio() {
-        return calcio;
-    }
-
-    public void setCalcio(Double calcio) {
-        this.calcio = calcio;
-    }
-
-    public Double getMagnesio() {
-        return magnesio;
-    }
-
-    public void setMagnesio(Double magnesio) {
-        this.magnesio = magnesio;
-    }
-
-    public Double getEnxofre() {
-        return enxofre;
-    }
-
-    public void setEnxofre(Double enxofre) {
-        this.enxofre = enxofre;
-    }
-
-    public Double getAluminio() {
-        return aluminio;
-    }
-
-    public void setAluminio(Double aluminio) {
-        this.aluminio = aluminio;
-    }
-
-    public Double getHAL() {
-        return HAL;
-    }
-
-    public void setHAL(Double HAL) {
-        this.HAL = HAL;
     }
 
     public Double getFosforoIdeal() {
---
+++ src/main/java/com/bruno/app/models/Teores.java
@@ -0,0 +1,67 @@
+package com.bruno.app.models;
+
+public class Teores {
+    private Double fosforo;
+    private Double potassio;
+    private Double calcio;
+    private Double magnesio;
+    private Double enxofre;
+    private Double aluminio;
+    private Double HAL;
+
+    public Double getFosforo() {
+        return fosforo;
+    }
+
+    public void setFosforo(Double fosforo) {
+        this.fosforo = fosforo;
+    }
+
+    public Double getPotassio() {
+        return potassio;
+    }
+
+    public void setPotassio(Double potassio) {
+        this.potassio = potassio;
+    }
+
+    public Double getCalcio() {
+        return calcio;
+    }
+
+    public void setCalcio(Double calcio) {
+        this.calcio = calcio;
+    }
+
+    public Double getMagnesio() {
+        return magnesio;
+    }
+
+    public void setMagnesio(Double magnesio) {
+        this.magnesio = magnesio;
+    }
+
+    public Double getEnxofre() {
+        return enxofre;
+    }
+
+    public void setEnxofre(Double enxofre) {
+        this.enxofre = enxofre;
+    }
+
+    public Double getAluminio() {
+        return aluminio;
+    }
+
+    public void setAluminio(Double aluminio) {
+        this.aluminio = aluminio;
+    }
+
+    public Double getHAL() {
+        return HAL;
+    }
+
+    public void setHAL(Double HAL) {
+        this.HAL = HAL;
+    }
+}
