0ce6b5fa5b1172b92ced31e6e225958f9de8cfbe
Bruno Ricardo Secco <brunoricardosecco@hotmail.com>
2021-03-31 02:00:42 -0300

    feat: adicao de classe de CorrecaoFosforo

---
+++ src/main/java/com/bruno/app/models/CorrecaoFosforo.java
@@ -0,0 +1,12 @@
+package com.bruno.app.models;
+
+public class CorrecaoFosforo {
+
+    public Double calculaQuantidadeParaAplicar(Double teorAAtingir, Integer fonteFosforo) {
+
+    }
+
+    public Double calculaP2O5() {
+
+    }
+}
