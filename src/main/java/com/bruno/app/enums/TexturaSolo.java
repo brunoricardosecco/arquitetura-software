7d8f33435dd9fb0cef0ebd9b5e8c8f3e2cef1f32
Bruno Ricardo Secco <brunoricardosecco@hotmail.com>
2021-03-31 00:00:02 -0300

    feat: adicionado enum de texturas de solos

---
+++ src/main/java/com/bruno/app/enums/TexturaSolo.java
@@ -0,0 +1,12 @@
+package com.bruno.app.enums;
+
+public enum TexturaSolo {
+    ARGILOSO(1),
+    TEXTURA_MEDIA(2);
+
+    private final Integer codigo;
+
+    TexturaSolo(Integer codigo) {
+        this.codigo = codigo;
+    }
+}
