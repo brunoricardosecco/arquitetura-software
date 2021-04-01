7cb7202eaa50215e6a98086889713ef173f887f5
Bruno Ricardo Secco <brunoricardosecco@hotmail.com>
2021-04-01 05:10:43 -0300

    feat: adicionado funcao para calcular quantidade necessaria para o potassio

--- src/main/java/com/bruno/app/models/CorrecaoPotassio.java
+++ src/main/java/com/bruno/app/models/CorrecaoPotassio.java
@@ -1,20 +1,23 @@
 package com.bruno.app.models;
 
 public class CorrecaoPotassio {
-    private Double teorPotassioAtualNaCTC;
     private Double ctcCmol;
     private Double teorPotassioDesejadoNaCTC;
     private Integer texturaSolo;
+    private Double teorAtualPotassioNoSolo;
+    private Integer fontePotassio;
+    private  final Double eficienciaPotassio = 0.85;
 
-    public CorrecaoPotassio(Double teorPotassioAtualNaCTC, Double ctcCmol, Double teorPotassioDesejadoNaCTC, Integer texturaSolo) {
-        this.teorPotassioAtualNaCTC = teorPotassioAtualNaCTC;
+    public CorrecaoPotassio(Double ctcCmol, Double teorPotassioDesejadoNaCTC, Integer texturaSolo, Double teorAtualPotassioNoSolo, Integer fontePotassio) {
         this.ctcCmol = ctcCmol;
         this.teorPotassioDesejadoNaCTC = teorPotassioDesejadoNaCTC;
         this.texturaSolo = texturaSolo;
+        this.teorAtualPotassioNoSolo = teorAtualPotassioNoSolo;
+        this.fontePotassio = fontePotassio;
     }
 
     public Double calculaParticipacaoAtualDoPotassioNaCTC() {
-        return teorPotassioAtualNaCTC / ctcCmol * 100;
+        return teorAtualPotassioNoSolo / ctcCmol * 100;
     }
 
     public Double calculaParticipacaoDoPotassioNaCTCAposCorrecao() {
@@ -31,4 +34,48 @@ public class CorrecaoPotassio {
                 return 0.0;
         }
     }
+
+    private Double calculaNecessidadeDeK() {
+        return (teorAtualPotassioNoSolo * teorPotassioDesejadoNaCTC / calculaParticipacaoAtualDoPotassioNaCTC()) - teorAtualPotassioNoSolo;
+    }
+
+    private Double calculaCmolDm3DeKEmMgDm3DeK() {
+        if (calculaNecessidadeDeK() < 0.01) {
+            return 0.0;
+        }
+        return calculaNecessidadeDeK() * 39.1 * 10;
+    }
+
+    private Double calculaMgDm3DeKEmKgHaDeK() {
+        return calculaCmolDm3DeKEmMgDm3DeK() * 2;
+    }
+
+    private Double calculaKgHaDeKEmK2O() {
+        return calculaMgDm3DeKEmKgHaDeK() * 1.2;
+    }
+
+    private Double calculaValorDeK2OConsiderandoEficienciaPotassio() {
+        return calculaKgHaDeKEmK2O() * 100 / eficienciaPotassio / 100;
+    }
+
+
+    private Double calculaTeorDeK2ODaFonteDePotassio() {
+        switch (fontePotassio) {
+            case 1:
+                return 58.0;
+            case 2:
+                return 52.0;
+            case 3:
+                return 22.0;
+            case 4:
+                return 44.0;
+            default:
+                return 0.0;
+        }
+    }
+
+    public Double calculaQuantidadeAAplicar() {
+        return calculaValorDeK2OConsiderandoEficienciaPotassio() * 100 / calculaTeorDeK2ODaFonteDePotassio();
+    }
+
 }
--- src/test/java/com/bruno/app/models/CorrecaoPotassioTest.java
+++ src/test/java/com/bruno/app/models/CorrecaoPotassioTest.java
@@ -4,7 +4,7 @@ import junit.framework.TestCase;
 
 public class CorrecaoPotassioTest extends TestCase {
     Double ctcCmol = new CorrecaoCTC().calculaCTCcmol(new CorrecaoCTC().calculaScmol(0.15, 5.56, 1.63), 5.35);
-    CorrecaoPotassio correcaoPotassioMock = new CorrecaoPotassio(0.15, ctcCmol, 7.0, 1);
+    CorrecaoPotassio correcaoPotassioMock = new CorrecaoPotassio(ctcCmol, 7.0, 1, 0.15, 3);
 
     public void testCalculaParticipacaoAtualDoPotassioNaCTC() {
         assertEquals(1.1820330969267139, correcaoPotassioMock.calculaParticipacaoAtualDoPotassioNaCTC());
@@ -16,5 +16,9 @@ public class CorrecaoPotassioTest extends TestCase {
 
     public void testCalculaParticipacaoDoPotassioIdealNaCTC() {
         assertEquals(3.0, correcaoPotassioMock.calculaParticipacaoPotassioIdealNaCTC());
+    }
+
+    public void testCalculaQuantidadeAAplicar() {
+        assertEquals(3775.18, correcaoPotassioMock.calculaQuantidadeAAplicar());
     }
 }
