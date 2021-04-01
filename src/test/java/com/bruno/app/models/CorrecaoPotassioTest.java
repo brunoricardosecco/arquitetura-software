f1ee8371fc460e58c9688ecdac276f5f9a0c5347
Bruno Ricardo Secco <brunoricardosecco@hotmail.com>
2021-04-01 04:13:17 -0300

    feat: test adicao de metodo para calcular participacao do potassio na ctc ideal

--- src/test/java/com/bruno/app/models/CorrecaoPotassioTest.java
+++ src/test/java/com/bruno/app/models/CorrecaoPotassioTest.java
@@ -4,7 +4,7 @@ import junit.framework.TestCase;
 
 public class CorrecaoPotassioTest extends TestCase {
     Double ctcCmol = new CorrecaoCTC().calculaCTCcmol(new CorrecaoCTC().calculaScmol(0.15, 5.56, 1.63), 5.35);
-    CorrecaoPotassio correcaoPotassioMock = new CorrecaoPotassio(0.15, ctcCmol, 7.0);
+    CorrecaoPotassio correcaoPotassioMock = new CorrecaoPotassio(0.15, ctcCmol, 7.0, 1);
 
     public void testCalculaParticipacaoAtualDoPotassioNaCTC() {
         assertEquals(1.1820330969267139, correcaoPotassioMock.calculaParticipacaoAtualDoPotassioNaCTC());
@@ -12,5 +12,9 @@ public class CorrecaoPotassioTest extends TestCase {
 
     public void testCalculaParticipacaoDoPotassioNaCTCAposCorrecao() {
         assertEquals(7.0, correcaoPotassioMock.calculaParticipacaoDoPotassioNaCTCAposCorrecao());
+    }
+
+    public void testCalculaParticipacaoDoPotassioIdealNaCTC() {
+        assertEquals(3.0, correcaoPotassioMock.calculaParticipacaoPotassioIdealNaCTC());
     }
 }
