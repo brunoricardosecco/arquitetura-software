ca4902ea3cfb2211a91c59e69c7315b855126223
Bruno Ricardo Secco <brunoricardosecco@hotmail.com>
2021-04-01 02:29:40 -0300

    feat: adicionado metodos para testar o custo da correcao e calculo de residuos

--- src/test/java/com/bruno/app/models/CorrecaoFosforoTest.java
+++ src/test/java/com/bruno/app/models/CorrecaoFosforoTest.java
@@ -3,8 +3,23 @@ package com.bruno.app.models;
 import junit.framework.TestCase;
 
 public class CorrecaoFosforoTest extends TestCase {
+    CorrecaoFosforo correcaoFosforoMock = new CorrecaoFosforo(12.0, 8.59, 70.0, 1, 1000.0);
 
     public void testCalculaQuantidadeAAplicar() {
-        assertEquals(123.95079365079366, new CorrecaoFosforo(12.0, 8.59, 70.0, 1).calculaQuantidadeAAplicar());
+        assertEquals(123.95079365079366, correcaoFosforoMock.calculaQuantidadeAAplicar());
+    }
+
+    public void testCalculaPrimeiroResidual() {
+        assertEquals(12.395079365079365, correcaoFosforoMock.calculaKgHaPrimeiroResiduo());
+        assertEquals("Enxofre", correcaoFosforoMock.calculaNomePrimeiroResiduo());
+    }
+
+    public void testCalculaSegundoResidual() {
+        assertEquals(34.70622222222223, correcaoFosforoMock.calculaKgHaSegundoResiduo());
+        assertEquals("Cálcio", correcaoFosforoMock.calculaNomeSegundoResiduo());
+    }
+
+    public void testCalculaPrecoPorToneladaFosforo() {
+        assertEquals(123.95079365079366, correcaoFosforoMock.calculaCustoPorHa());
     }
 }
